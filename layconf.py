651103d3f757e3f3104b94a1183c4527276713d4
davidoh <david.ohana@ibm.com>
2020-05-20 17:37:42 +0300

    initial commit

---
+++ layconf.py
@@ -0,0 +1,111 @@
+import configparser
+import os
+
+
+class LayConf:
+    _config_parser_default = configparser.ConfigParser()
+    _config_parser_custom = configparser.ConfigParser()
+    env_prefix = ""
+
+    ERROR_WHEN_NOT_FOUND = object
+    BOOLEAN_STATES = {'1': True, 'yes': True, 'true': True, 'on': True,
+                      '0': False, 'no': False, 'false': False, 'off': False}
+
+    @staticmethod
+    def init_config(default_config_file_path="cfg/default.ini", custom_config_file_path="", env_prefix=""):
+        assert not is_none_or_blank(default_config_file_path)
+        if env_prefix is None:
+            env_prefix = ""
+
+        LayConf.env_prefix = env_prefix
+        LayConf._config_parser_default.read(default_config_file_path)
+        if not is_none_or_blank(custom_config_file_path):
+            LayConf._config_parser_custom.read(custom_config_file_path)
+
+        print("config env prefix:", env_prefix)
+        print("config default:", default_config_file_path)
+        print("config custom:", custom_config_file_path)
+
+    @staticmethod
+    def get(section: str, option: str, fallback: object = ERROR_WHEN_NOT_FOUND):
+        assert not is_none_or_blank(section)
+        assert not is_none_or_blank(option)
+
+        env_key = LayConf._get_env_key(section, option)
+        env_val = os.environ.get(env_key)
+        if env_val is not None:
+            return env_val
+
+        custom_val = LayConf._config_parser_custom.get(section, option, fallback=None)
+        if custom_val is not None:
+            return custom_val
+
+        default_val = LayConf._config_parser_default.get(section, option, fallback=None)
+        if default_val is not None:
+            return default_val
+
+        if fallback == LayConf.ERROR_WHEN_NOT_FOUND:
+            raise KeyError(f"Configuration value for {section}/{option} not found")
+
+        return fallback
+
+    @staticmethod
+    def getint(section: str, option: str, fallback: object = ERROR_WHEN_NOT_FOUND):
+        str_value = LayConf.get(section, option, fallback)
+        return int(str_value)
+
+    @staticmethod
+    def getfloat(section: str, option: str, fallback: object = ERROR_WHEN_NOT_FOUND):
+        str_value = LayConf.get(section, option, fallback)
+        return float(str_value)
+
+    @staticmethod
+    def getboolean(section: str, option: str, fallback: object = ERROR_WHEN_NOT_FOUND):
+        str_value = LayConf.get(section, option, fallback)
+        return LayConf._convert_to_boolean(str_value)
+
+    @staticmethod
+    def getsection(section: str):
+        assert not is_none_or_blank(section)
+        return Section(section)
+
+    @staticmethod
+    def _get_env_key(section, option):
+        tokens = []
+        if LayConf.env_prefix:
+            tokens.append(LayConf.env_prefix)
+        tokens.append(section)
+        tokens.append(option)
+        return '_'.join(tokens)
+
+    @staticmethod
+    def _convert_to_boolean(value):
+        """Return a boolean value translating from other types if necessary.
+        """
+        if value.lower() not in LayConf.BOOLEAN_STATES:
+            raise ValueError('Not a boolean: %s' % value)
+        return LayConf.BOOLEAN_STATES[value.lower()]
+
+
+def is_none_or_blank(s: str):
+    return s is None or str(s) == ""
+
+
+class Section:
+    def __init__(self, section: str):
+        self.section = section
+
+    def __getitem__(self, option: str):
+        return LayConf.get(self.section, option)
+
+    def get(self, option: str, fallback=None):
+        return LayConf.get(self.section, option, fallback)
+
+    def getint(self, option: str, fallback: object = LayConf.ERROR_WHEN_NOT_FOUND):
+        return LayConf.getint(self.section, option, fallback)
+
+    def getfloat(self, option: str, fallback: object = LayConf.ERROR_WHEN_NOT_FOUND):
+        return LayConf.getfloat(self.section, option, fallback)
+
+    def getboolean(self, option: str, fallback: object = LayConf.ERROR_WHEN_NOT_FOUND):
+        return LayConf.getboolean(self.section, option, fallback)
---
+++ readme.md
@@ -0,0 +1,54 @@
+## LayConf - Layered Configuration Parser for Python3
+
+### Overview
+
+A simple wrapper over Python's configparsr that reads configuration in the following order:
+
+1. Environment variables
+1. Custom configuration ini file  (optional)
+1. Default configuration ini file (must exist)
+
+Each config option lookup will fallback to next layer if not found in the present layer.
+
+Convention for env. vars is `{prefix}_{section}_{option}` or `{section}_{option}` if prefix is empty. For example: `LOG_file_backup_count=100`
+
+All lookups are case sensitive.    
+ 
+
+ ### Example
+```python
+from layconf import LayConf
+
+# the default config file is cfg/default.ini unless specified other
+# custom_config_file_path is an optional layer
+# env_prefix is empty by default, which means that environment variable names will be mapped to {section}_{option}
+LayConf.init_config(custom_config_file_path="cfg/staging.ini", env_prefix="example")
+
+print("env_name:", LayConf.get("DATABASE", "env_name"))
+print("console_enabled:", LayConf.getboolean("LOG", "console_enabled"))
+print("file_rotation_size_mb:", LayConf.getint("LOG", "file_rotation_size_mb"))
+print("file_rotation_size_mb:", LayConf.getint("LOG", "file_rotation_size_mb"))
+
+# dealing with not exist keys
+# be default - raise an Error, as it is recommended to keep defaults for all fields in default.ini
+try:
+    print("foo:", LayConf.get("FOO", "foo"))
+except KeyError:
+    print("'foo' not found")
+# but it is also possible to specify an inline default
+print("foo:", LayConf.get("FOO", "foo", fallback="bar"))
+print("foo_number:", LayConf.get("FOO", "foo_number", fallback=33))
+
+# also possible to get an entire config section and read from with an indexer
+log_section = LayConf.getsection("LOG")
+print("file_enabled:", log_section["file_enabled"])
+print("file_backup_count:", log_section.getint("file_backup_count"))
+```
+
+### ToDo:
+
+* Packaging to pip
+ 
+### License: 
+
+Apache V2
